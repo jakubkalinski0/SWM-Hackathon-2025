2708966704df754a7b1b7a5d79a55ca4dd2d618d
Massny <pawel.fornagiel2@gmail.com>
2025-03-29 13:13:09 +0100

    add barcode scanner component

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -3,13 +3,16 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {https: {}},
+  plugins: [react(), mkcert()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  
 })
